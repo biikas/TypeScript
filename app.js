21af268781ebcf0d4a35ee39e1eec07f9bd7669f
bikas <bikas.shah1921@gmail.com>
2022-01-04 08:33:51 +0545

    typescript types

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("My first typescript code");
+//variable declaration
+var fname = "Bikash";
+var address = "Biratnagar";
+var pi = 3.14;
+var g = 9.98;
+console.log(fname);
+console.log(address);
+console.log(pi);
+console.log(g);
---
+++ app.ts
@@ -0,0 +1,14 @@
+console.log("My first typescript code");
+
+//variable declaration
+ var fname="Bikash";
+ let address="Biratnagar";
+ 
+ const pi=3.14;
+ const g =9.98;
+
+
+ console.log(fname);
+ console.log(address);
+ console.log(pi);
+ console.log(g);
